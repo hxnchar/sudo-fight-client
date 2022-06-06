d47df007ab505c307177e4387c0d9bce0c496131
Aleksey <alekseyhonchar@gmail.com>
2022-06-06 19:47:20 +0100

    Linters added

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: '*.ts',
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        'import/prefer-default-export': 'off',
+      },
+    },
+  ],
+};
