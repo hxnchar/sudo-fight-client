cba6053da4de6602c0e16213dd828d7952e2f6a5
Aleksey <alekseyhonchar@gmail.com>
2022-06-21 22:49:25 +0100

    feat/login-page-added

---
+++ src/components/pages/Login/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Login.module.scss';
+import CInput from 'components/basic/CInput';
+import CButton, { ButtonDecor } from 'components/basic/CButton';
+
+const Login = () => {
+  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //some logic would be here soon
+  };
+  return (
+    <div className={styles.container}>
+      <form onSubmit={onSubmit}>
+        <h1>Welcome</h1>
+        <div className={styles.inputs}>
+          <CInput label={'Username'} />
+          <CInput label={'Password'} type={'password'} />
+          <a href="sign-up" className={`${styles['forgot-password']}`}>
+            Forgot Password
+          </a>
+        </div>
+        <CButton decor={ButtonDecor.Filled}>Login</CButton>
+        <div className={styles['elements-inline']}>
+          <p>Not a member yet?</p>
+          <a href="sign-up">Sign Up</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
