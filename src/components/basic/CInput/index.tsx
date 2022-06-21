957019ba1651e36f520451a6e3c4436aca982ebb
Aleksey <alekseyhonchar@gmail.com>
2022-06-21 19:51:59 +0100

    refactor/added-basic-folder

--- src/components/basic/CInput/index.tsx
+++ src/components/basic/CInput/index.tsx
@@ -1,17 +1,17 @@
-import React, { InputHTMLAttributes, useState } from 'react';
+import React, { ComponentProps, InputHTMLAttributes, useState } from 'react';
 import styles from './CInput.module.scss';
 
-const Index = ({ label, ...props }: InputProps) => {
+const Index = ({ label, ...props }: InputProps & ComponentProps<'input'>) => {
   const [text, setText] = useState('');
-
+  const ref = React.createRef<HTMLInputElement>();
   const onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
-  const onClickInput = () => document.getElementById('input')?.focus();
+  const onClickInput = () => ref.current?.focus();
 
   return (
     <div className={`${styles.container} ${text ? styles['has-text'] : ''}`} onClick={onClickInput}>
       <label>{label}</label>
-      <input {...props} onChange={onTextChange} id="input" />
+      <input {...props} onChange={onTextChange} id="input" ref={ref} />
     </div>
   );
 };
