90301cd9e73ec3b53fc643d682a8a7f664179c09
Aleksey <alekseyhonchar@gmail.com>
2022-06-22 09:01:20 +0100

    feat/register-page-added

---
+++ src/components/pages/Register/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../Login/Login.module.scss';
+import CInput from 'components/basic/CInput';
+import CButton, { ButtonDecor } from 'components/basic/CButton';
+
+const Login = () => {
+  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //some logic would be here soon
+  };
+  return (
+    <div className={styles.container}>
+      <form onSubmit={onSubmit}>
+        <h1>Registration</h1>
+        <div className={styles.inputs}>
+          <CInput label={'First name'} />
+          <CInput label={'Last name'} />
+          <CInput label={'Email'} />
+          <CInput label={'Password'} type={'password'} />
+          <CInput label={'Confirm password'} type={'password'} />
+        </div>
+        <CButton decor={ButtonDecor.Filled}>Continue</CButton>
+        <div className={styles['elements-inline']}>
+          <p>Already a member?</p>
+          <a href="login">Login</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
