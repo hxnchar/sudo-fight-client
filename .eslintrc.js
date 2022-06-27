7c65582a632d143d61ac7f6f219375f119210d29
Aleksey <alekseyhonchar@gmail.com>
2022-06-27 11:15:19 +0100

    feat/added-metarhia-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 module.exports = {
   env: {
-    node: true,
     browser: true,
     es2021: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'metarhia',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'max-len': ['error', { 'code': 120 }],
   },
   overrides: [
     {
-      files: '*.{ts,tsx}',
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended',
+      files: [
+        '*.ts',
+        '*.tsx'
       ],
-      rules: {
-        'import/prefer-default-export': 'off',
-      },
-    },
+    }
   ],
 };
