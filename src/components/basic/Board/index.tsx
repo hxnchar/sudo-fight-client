91be4547aa65e2cbdf2d17c7eeb31eda7c0b9da0
Aleksey <alekseyhonchar@gmail.com>
2022-06-28 19:01:09 +0100

    feat/added-border-component

---
+++ src/components/basic/Board/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styles from './Board.module.scss';
+
+const Board = () => {
+  const cellSize = 9;
+  const siteSize = Math.sqrt(cellSize);
+  const ref = React.createRef<HTMLDivElement>();
+
+  const smallCellClicked = (e: React.MouseEvent<HTMLDivElement>) => {
+    const bigCellIndex = e.currentTarget.getAttribute('big-index');
+    const rowIndex = e.currentTarget.getAttribute('global-row');
+    const columnIndex = e.currentTarget.getAttribute('global-column');
+    const allBigCells: Array<HTMLElement> = [].slice.call(
+      e.currentTarget.parentElement?.parentElement?.children,
+    );
+    const allSmallCells: Array<HTMLElement> = [];
+    allBigCells.forEach((elem) => {
+      allSmallCells.push(...[].slice.call(elem.children));
+    });
+    allSmallCells.forEach((elem) => {
+      elem.setAttribute('nums-hint', 'false');
+      elem.setAttribute('is-selected', 'false');
+      elem.setAttribute('is-highlighted', 'false');
+    });
+    const cellsToBeHighlighted = allSmallCells.filter(
+      (elem) =>
+        elem.getAttribute('global-row') === rowIndex ||
+        elem.getAttribute('global-column') === columnIndex ||
+        elem.getAttribute('big-index') === bigCellIndex,
+    );
+    cellsToBeHighlighted.forEach((elem) => elem.setAttribute('is-highlighted', 'true'));
+    const numToFind: number =
+      e.currentTarget.innerText.trim() === '' ? -1 : Number(e.currentTarget.innerText);
+    console.log(numToFind);
+    const hintNums = allSmallCells.filter((elem) => Number(elem.innerText) === numToFind);
+    hintNums.forEach((elem) => elem.setAttribute('nums-hint', 'true'));
+    e.currentTarget.setAttribute('is-selected', 'true');
+  };
+  const cellsArray = [];
+  let fixedGlobalRow = 0;
+  for (let i = 0, globalRow = 0, globalColumn = 0; i < cellSize; i++) {
+    const tempArray = [];
+    if (i !== 0 && i % siteSize === 0) fixedGlobalRow += siteSize;
+    globalColumn = i % siteSize === 0 ? 0 : globalColumn;
+    let fixedGlobalColumn = globalColumn;
+    for (let j = 0; j < cellSize; j++) {
+      fixedGlobalColumn = j === cellSize - 1 ? globalColumn : fixedGlobalColumn;
+      globalColumn = j % siteSize === 0 ? fixedGlobalColumn : globalColumn;
+      if (j === 0) globalRow = fixedGlobalRow;
+      else if (j !== 0 && j % siteSize === 0) globalRow += 1;
+      tempArray.push(
+        <div
+          className={styles.cell}
+          big-index={i}
+          small-index={j}
+          global-row={globalRow}
+          global-column={globalColumn++}
+          onClick={smallCellClicked}
+          is-highlighted={'false'}
+          is-selected={'false'}
+          hint-num={'false'}
+          key={`${globalRow};${globalColumn}`}
+        ></div>,
+      );
+    }
+    cellsArray.push(
+      <div className={styles['big-cell']} key={`${i}`}>
+        {tempArray}
+      </div>,
+    );
+  }
+  return (
+    <div className={styles.container} ref={ref}>
+      {cellsArray}
+    </div>
+  );
+};
+
+export default Board;
--- src/components/basic/index.ts
+++ src/components/basic/index.ts
@@ -1,2 +1,3 @@
 export { default as CButton } from './CButton';
 export { default as CInput } from './CInput';
+export { default as Border } from './Board';
