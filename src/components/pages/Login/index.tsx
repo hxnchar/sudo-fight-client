7904f21c634ff04dd6db8646f4245d587ab73854
Aleksey <alekseyhonchar@gmail.com>
2022-07-18 16:47:41 +0100

    feat/added-basic-auth-logic

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.scss';
 import { CInput, CButton } from 'components';
+import axios from 'axios';
 
 const Login = () => {
-  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //some logic would be here soon
+    try {
+      await axios.post(`${process.env.REACT_APP_DOMAIN}/login`, {
+        username,
+        password,
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      //loaders logic
+    }
   };
   return (
     <div className={styles.container}>
       <form onSubmit={onSubmit} role={'form'}>
         <h1>Welcome</h1>
         <div className={styles.inputs}>
-          <CInput label={'Username'} />
-          <CInput label={'Password'} type={'password'} />
+          <CInput label={'Username'}
+            onChange={(e) => setUsername(e.target.value)}/>
+          <CInput label={'Password'} type={'password'}
+            onChange={(e) => setPassword(e.target.value)} />
           <a href="sign-up" className={`${styles['forgot-password']}`}>
             Forgot Password
           </a>
--- src/components/pages/Register/index.tsx
+++ src/components/pages/Register/index.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../Login/Login.module.scss';
 import { CInput } from 'components';
 import { CButton } from 'components';
+import axios from 'axios';
 
 const Login = () => {
-  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const usernameIsValid = (username: string) => username.match(/(?=.{4,15}$)[[0-9a-zA-Z]/);
+  const passwordIsValid = (password: string) => password.match(/(?=.{8,20}$)[[0-9a-zA-Z]/);
+  const onSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //some logic would be here soon
+    try {
+      if (usernameIsValid(username) &&
+        passwordIsValid(password) &&
+        password === confirmPassword) {
+        await axios.post(`${process.env.REACT_APP_DOMAIN}/register`, {
+          username,
+          email,
+          password,
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      //loaders logic
+    }
   };
   return (
     <div className={styles.container}>
       <form onSubmit={onSubmit} role={'form'}>
         <h1>Registration</h1>
         <div className={styles.inputs}>
-          <CInput label={'Username'} />
-          <CInput label={'Email'} />
-          <CInput label={'Password'} type={'password'} />
-          <CInput label={'Confirm password'} type={'password'} />
+          <CInput label={'Username'}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }} />
+          <CInput label={'Email'}
+            onChange={(e) => setEmail(e.target.value)} />
+          <CInput label={'Password'} type={'password'}
+            onChange={(e) => setPassword(e.target.value)} />
+          <CInput label={'Confirm password'} type={'password'}
+            onChange={(e) => setConfirmPassword(e.target.value)} />
         </div>
         <CButton decor={'filled'}>Continue</CButton>
         <div className={styles['elements-inline']}>
